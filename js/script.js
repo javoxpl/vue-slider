e728c108372cb234e746f9dbd69c9f2fd393520b
javoxpl <liacovone7@outlook.it>
2022-06-12 15:32:25 +0200

    exercise completed

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const myApp = new Vue({
+    el: "#app",
+    data : {
+        activeSlide : 0,
+        slides : [
+            {
+                image: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                image: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum.',
+            },
+            {
+                image: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                image: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            },
+            {   
+                image: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+            }   
+        ]
+    },
+    methods : {
+        nextSlides(){
+            this.activeSlide++;
+            console.log(this.activeSlide);
+            if( this.activeSlide > this.slides.length - 1) {
+                this.activeSlide = 0;
+            }
+        },
+        prevSlides(){
+            this.activeSlide--;
+            if( this.activeSlide < 0) {
+                this.activeSlide = this.slides.length -1;
+            }
+        },
+        cambioSlide(numeroSlide){
+            this.activeSlide = numeroSlide;
+        }
+        
+    }
+});
+
+
